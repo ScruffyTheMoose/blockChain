eb7c963ad38a025b9776caf5c4478f87fbd9077e
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2022-05-21 14:42:18 -0500

    add consensus

--- chain.py
+++ chain.py
@@ -1,6 +1,8 @@
 import hashlib
 import time
 import json
+import requests
+from urllib.parse import urlparse
 
 
 class Blockchain:
@@ -8,6 +10,7 @@ class Blockchain:
     def __init__(self) -> None:
         self.chain = list()
         self.transactions = list()
+        self.nodes = set()
 
         # genesis block
         self.newBlock(proof=100, prevHash=1)
@@ -16,7 +19,7 @@ class Blockchain:
         block = {
             "index": len(self.chain) + 1,
             "timestamp": time.time(),
-            "transactions": list(),
+            "transactions": self.transactions,
             "proof": proof,
             "prev_hash": prevHash,
         }
@@ -72,3 +75,50 @@ class Blockchain:
 
         # good proof key produced hash with 4 leading zeros
         return hashed[:4] == "0000"
+
+    def registerNode(self, address):
+        parsedURL = urlparse(address)
+        self.nodes.add(parsedURL)
+
+    def verifyChain(self, chain):
+        prevBlock = chain[0]
+        currentIdx = 1
+
+        while currentIdx < len(chain):
+            block = chain[currentIdx]
+
+            # validating blocks are hashed and ordered correctly
+            if block['prev_hash'] != self.hash(prevBlock):
+                return False
+
+            if not self.verifyProof(prevBlock['proof'], block['proof']):
+                return False
+
+            prevBlock = block
+            currentIdx += 1
+
+        return True
+
+    def resolveConflicts(self):
+        neighbors = self.nodes
+        newChain = None
+
+        # length of our chain
+        maxLength = len(self.chain)
+
+        for node in neighbors:
+            response = requests.get(f'{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > maxLength and self.verifyChain(chain):
+                    maxLength = length
+                    newChain = chain
+
+        if newChain:
+            self.chain = newChain
+            return True
+
+        return False
