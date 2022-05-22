b4739d201b16a442f377a73703db7b1ffe615f8f
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2022-05-22 14:46:03 -0500

    add comments/method descriptions

--- app.py
+++ app.py
@@ -8,73 +8,87 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 # unique address for this node
-node_id = str(uuid4()).replace('-', '')
+node_id = str(uuid4()).replace("-", "")
 
 # instantiate blockchain
 blockchain = Blockchain()
 
 
-@app.route('/mine', methods=['GET'])
+@app.route("/mine", methods=["GET"])
 def mine():
+    """[GET] - Mines a new Block"""
+
+    # getting info from the last Block
     prevBlock = blockchain.tail
-    prevProof = prevBlock['proof']
+    prevProof = prevBlock["proof"]
+
+    # finding proof key for Block being mined
     proof = blockchain.pow(prevProof)
 
+    # awarding one unit for successfully mined Block
     blockchain.newTrans(
-        sender='0',
+        sender="0",
         recipient=node_id,
         amount=1,
     )
 
+    # creating the new Block and adding it to the chain
     prevHash = blockchain.hash(prevBlock)
     block = blockchain.newBlock(proof, prevHash)
 
+    # response to be passed back to node
     response = {
-        'message': "New Block forged",
-        'index': block['index'],
-        'transactions': block['transactions'],
-        'proof': proof,
-        'prev_hash': block['prev_hash'],
+        "message": "New Block forged",
+        "index": block["index"],
+        "transactions": block["transactions"],
+        "proof": proof,
+        "prev_hash": block["prev_hash"],
     }
 
     return jsonify(response), 200
 
 
-@app.route('/transactions/new', methods=['POST'])
+@app.route("/transactions/new", methods=["POST"])
 def newTrans():
+    """[POST] - Creates a new transaction on the current Block"""
+
     # getting the JSON object that was pushed
     args = request.get_json()
-    reqs = ['sender', 'recipient', 'amount']
+    reqs = ["sender", "recipient", "amount"]
 
     # verifying required 3 arguments were given
     if not all(k in args for k in reqs):
-        return 'Missing arguments', 400
+        return "Missing arguments", 400
 
     # creating new transaction which returns the index of the block where the transaction will be stored
     idx = blockchain.newTrans(
-        sender=args['sender'],
-        recipient=args['recipient'],
-        amount=args['amount'],
+        sender=args["sender"],
+        recipient=args["recipient"],
+        amount=args["amount"],
     )
 
-    response = {'message': f'Transaction will be added to Block {idx}'}
+    response = {"message": f"Transaction will be added to Block {idx}"}
     return jsonify(response), 201
 
 
-@app.route('/chain', methods=['GET'])
+@app.route("/chain", methods=["GET"])
 def fullChain():
+    """[GET] - Produces the entire chain"""
+
     response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
+        "chain": blockchain.chain,
+        "length": len(blockchain.chain),
     }
 
     return jsonify(response), 200
 
 
-@app.route('/nodes/register', methods=['POST'])
+@app.route("/nodes/register", methods=["POST"])
 def registerNodes():
+    """[POST] - Register a new node on the chain"""
+
     args = request.get_json()
-    nodes = args['nodes']
+    nodes = args["nodes"]
 
     if nodes is None:
         return "Error: Please supply a valid list of Nodes", 400
@@ -83,32 +97,28 @@ def registerNodes():
         blockchain.registerNode(node)
 
     response = {
-        'message': "New nodes have been registered",
-        'nodes': list(blockchain.nodes)
+        "message": "New nodes have been registered",
+        "nodes": list(blockchain.nodes),
     }
 
     return jsonify(response), 201
 
 
-@app.route('/nodes/resolve', methods=['GET'])
+@app.route("/nodes/resolve", methods=["GET"])
 def consensus():
+    """[GET] - Resolves any existing conflicts and ensures consensus across the chain"""
+
     replaced = blockchain.resolveConflicts()
 
     if replaced:
-        response = {
-            'message': "Our chain was replaced",
-            'chain': blockchain.chain
-        }
+        response = {"message": "Our chain was replaced", "chain": blockchain.chain}
 
     else:
-        response = {
-            'message': "Our chain is authoritative",
-            'chain': blockchain.chain
-        }
+        response = {"message": "Our chain is authoritative", "chain": blockchain.chain}
 
     return jsonify(response), 200
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     portVal = sys.argv[1]
-    app.run(host='0.0.0.0', port=portVal)
+    app.run(host="0.0.0.0", port=portVal)
--- chain.py
+++ chain.py
@@ -6,7 +6,6 @@ from urllib.parse import urlparse
 
 
 class Blockchain:
-
     def __init__(self) -> None:
         self.chain = list()
         self.transactions = list()
@@ -16,6 +15,16 @@ class Blockchain:
         self.newBlock(proof=100, prevHash=1)
 
     def newBlock(self, proof, prevHash) -> dict:
+        """Constructs a new Block
+
+        Args:
+            proof (int): Proof key used for the hash
+            prevHash (str): Hash of previous Block for verification
+
+        Returns:
+            dict: The data being stored in the Block
+        """
+
         block = {
             "index": len(self.chain) + 1,
             "timestamp": time.time(),
@@ -34,17 +43,39 @@ class Blockchain:
         return block
 
     def newTrans(self, sender, recipient, amount) -> int:
-        self.transactions.append({
-            "sender": sender,
-            "recipient": recipient,
-            "amoumnt": amount,
-        })
+        """Create a new transaction for the current Block
+
+        Args:
+            sender (str): Node address of sender
+            recipient (str): Node address of recipient
+            amount (int): Quantity of units being sent
+
+        Returns:
+            int: Index of the Block where this transaction will be stored
+        """
+
+        self.transactions.append(
+            {
+                "sender": sender,
+                "recipient": recipient,
+                "amoumnt": amount,
+            }
+        )
 
         # returning index of block where this transaction will be stored
         return self.tail["index"] + 1
 
     @staticmethod
     def hash(block) -> str:
+        """To hash a Block
+
+        Args:
+            block (dict): The Block that will be hashed
+
+        Returns:
+            str: Resulting hash
+        """
+
         # convert block into JSON formatted string with UTF-8 encoding
         blockString = json.dumps(block, sort_keys=True).encode()
 
@@ -55,9 +86,26 @@ class Blockchain:
 
     @property
     def tail(self) -> dict:
+        """The last Block in the chain
+
+        Returns:
+            dict: The data stored in the last Block
+        """
+
         return self.chain[-1]
 
-    def pow(self, lastProof):
+    def pow(self, lastProof) -> int:
+        """Proof of Work to identify the correct proof key for a Block
+        In this case, want to find a new proof key such that when concatenated with the previous
+        proof key, the new hash has three leading zeros.
+
+        Args:
+            lastProof (int): The previous Block's proof key
+
+        Returns:
+            int: New proof key
+        """
+
         # attempt key
         proof = 0
 
@@ -69,7 +117,17 @@ class Blockchain:
         return proof
 
     @staticmethod
-    def verifyProof(lastProof, proof):
+    def verifyProof(lastProof, proof) -> bool:
+        """Verifies that the given proof key is valid.
+
+        Args:
+            lastProof (int): Previous Block's proof key
+            proof (int): Current proof key attempt
+
+        Returns:
+            bool: True when proof key is valid, else False
+        """
+
         attempt = f"{lastProof}{proof}".encode()
         hashed = hashlib.sha256(attempt).hexdigest()
 
@@ -77,10 +135,25 @@ class Blockchain:
         return hashed[:4] == "0000"
 
     def registerNode(self, address):
+        """Registers a new node on the chain
+
+        Args:
+            address (str): The network address of the new node
+        """
+
         parsedURL = urlparse(address)
         self.nodes.add(parsedURL.netloc)
 
-    def verifyChain(self, chain):
+    def verifyChain(self, chain) -> bool:
+        """Verifies that the current chain is valid by checking all hashes and proof keys are valid
+
+        Args:
+            chain (list): The entire stored Blockchain on this node
+
+        Returns:
+            bool: True if chain is valid, else False
+        """
+
         prevBlock = chain[0]
         currentIdx = 1
 
@@ -88,10 +161,10 @@ class Blockchain:
             block = chain[currentIdx]
 
             # validating blocks are hashed and ordered correctly
-            if block['prev_hash'] != self.hash(prevBlock):
+            if block["prev_hash"] != self.hash(prevBlock):
                 return False
 
-            if not self.verifyProof(prevBlock['proof'], block['proof']):
+            if not self.verifyProof(prevBlock["proof"], block["proof"]):
                 return False
 
             prevBlock = block
@@ -99,7 +172,16 @@ class Blockchain:
 
         return True
 
-    def resolveConflicts(self):
+    # must also verify that all nodes share identical node lists
+    # resolving conflicts should be called for all nodes when any node builds a new block or a new node is added
+    # this would ensure there are never conflicts between two different chains
+    def resolveConflicts(self) -> bool:
+        """Resolves all conflicts across the Blockchain to ensure consensus of data
+
+        Returns:
+            bool: True if conflicts existed and were resolved, else False for no changes made
+        """
+
         neighbors = self.nodes
         newChain = None
 
@@ -107,11 +189,11 @@ class Blockchain:
         maxLength = len(self.chain)
 
         for node in neighbors:
-            response = requests.get(f'http://{node}/chain')
+            response = requests.get(f"http://{node}/chain")
 
             if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
+                length = response.json()["length"]
+                chain = response.json()["chain"]
 
                 if length > maxLength and self.verifyChain(chain):
                     maxLength = length
